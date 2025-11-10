69a1d6f472cab6338c74468f7cef446e07a56007
marcoa957 <marcoa957@gmail.com>
2025-11-10 15:59:23 -0300

    Menu interativo - Listar e Renomear
    Criação do menu interativo onde o usuário conseguirá listar e renomear os arquivos presentes no diretório.

---
+++ full_code.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+
+
+def listar_documentos(pasta): # Função que busca o caminho da pasta
+    arquivos_por_tipo = {} # Cria um dicionário vazio para agrupar os arquivos por tipo
+    for raiz, _, arquivos in os.walk(pasta): # Percorre cara raiz (caminho) e arquivo (com a extenção) da pasta
+        for arquivo in arquivos: # Para cada nome de arquivo dentro da pasta
+            ext = arquivo.split('.')[-1] # Fará um split separando por ponto e retornará apenas a ultima parte (extenção do arquivo)
+            arquivos_por_tipo.setdefault(ext, []).append(os.path.join(arquivo)) # Cria uma chave de extensão caso seja uma extensão nova e adiciona o nome dos arquivos relacionados
+    for tipo, arquivos in arquivos_por_tipo.items(): # Para cada tipo e arquivo dentro dos arquivos presentes na variavel resultado
+        print(f"\nArquivos .{tipo}:") # Printará um título com a extenção
+        for a in arquivos:
+            print(f" - {a}") # e o nome dos próprios arquivos da biblioteca
+
+def renomear_documento(): # Função para renomear o arquivo
+    arquivo = input("Digite o nome do arquivo que deseja editar (com a extenção desejada): ") # Instrução para o usuário apontar qual documento deverá ser renomeado
+    novo_nome = input("Qual nome deseja dar ao aquivo (com a devida extenção)? ") # Instrução para o usuário apontar qual será o novo nome do arquivo
+
+    caminho_antigo = f"biblioteca/{arquivo}" # Inicializa variável com o caminho antigo
+    caminho_novo = f"biblioteca/{novo_nome}" # Inicializa variável com o novo caminho
+
+    os.rename(caminho_antigo, caminho_novo) # Renomeia arquivo
+    print(f"Arquivo renomeado com sucesso para: {novo_nome}") # Mensagem de confirmação ao usuário
+
+def menu(): 
+    while True:
+        print("\n=== Sistema de Biblioteca Digital ===")
+        print("1. Listar documentos")
+        print("2. Adicionar documento")
+        print("3. Renomear documento")
+        print("4. Remover documento")
+        print("5. Sair")
+
+        opcao = input("Escolha uma opção:")
+
+
+        if opcao == '1':
+            listar_documentos("biblioteca")
+
+#        elif opcao == '2':
+#            adicionar_documento()
+        elif opcao == '3':
+            renomear_documento()
+#        elif opcao == '4':
+#            remover_documento()
+        elif opcao == '5':
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida!")
+
+if __name__ == "__main__":
+    menu()
