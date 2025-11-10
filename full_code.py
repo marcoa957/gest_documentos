ea803d9c3441239f4d20991717e6af3a6608894c
marcoa957 <marcoa957@gmail.com>
2025-11-10 18:09:59 -0300

    Melhorias Sugeridas
    Código completo contendo as melhorias captadas através do relatório de testes e feedbacks, como:
    criação de mensagens de erro e opção de retornar ao menu principal a qualquer momento da interação.

--- full_code.py
+++ full_code.py
@@ -14,23 +14,47 @@ def listar_documentos(pasta): # Função que busca o caminho da pasta
             print(f" - {a}") # e o nome dos próprios arquivos da biblioteca
 
 def renomear_documento(): # Função para renomear o arquivo
-    arquivo = input("Digite o nome do arquivo que deseja editar (com a extenção desejada): ") # Instrução para o usuário apontar qual documento deverá ser renomeado
-    novo_nome = input("Qual nome deseja dar ao aquivo (com a devida extenção)? ") # Instrução para o usuário apontar qual será o novo nome do arquivo
+    arquivo = input("Digite o nome do arquivo que deseja editar (com a extenção desejada) ou digite 'Menu' para retornar ao menu principal: ") # Instrução para o usuário apontar qual documento deverá ser renomeado
     caminho_antigo = f"biblioteca/{arquivo}" # Inicializa variável com o caminho antigo
-    caminho_novo = f"biblioteca/{novo_nome}" # Inicializa variável com o novo caminho
-    os.rename(caminho_antigo, caminho_novo) # Renomeia arquivo
-    print(f"Arquivo renomeado com sucesso para: {novo_nome}") # Mensagem de confirmação ao usuário
+    
+    if arquivo.strip().lower() == "menu":
+        return menu()
+    
+    elif not os.path.exists(caminho_antigo):
+        print("Não foi possível encontrar o arquivo indicado. Verifique e tente novamente")
+        return renomear_documento()
+    
+    else:   
+        novo_nome = input("Digite o novo nome que deseja dar ao arquivo (com a devida extenção) ou digite 'Menu' para retornar ao menu principal: ") # Instrução para o usuário apontar qual será o novo nome do arquivo
+        if novo_nome.strip().lower() == "menu":
+            return menu()
+        
+        caminho_novo = f"biblioteca/{novo_nome}" # Inicializa variável com o novo caminho
+            
+        os.rename(caminho_antigo, caminho_novo) # Renomeia arquivo
+        print(f"Arquivo renomeado com sucesso para: {novo_nome}") # Mensagem de confirmação ao usuário
 
 def adicionar_documento(): # Função para adicionar um novo arquivo
-    arquivo = input("Digite o nome do arquivo que deseja adicionar (com a extenção desejada): ") # Comando para o usuário escrever o nome do arquivo que será adicionado (com a extenção)
+    arquivo = input("Digite o nome do arquivo que deseja adicionar (com a extenção desejada) ou digite 'Menu' para retornar ao menu principal: ") # Comando para o usuário escrever o nome do arquivo que será adicionado (com a extenção)
+    if arquivo.strip().lower() == "menu":
+        return menu()   
+    
     novo_arquivo = f"biblioteca/{arquivo}" # Inicializa variável com o novo caminho.
     open(novo_arquivo, 'w').close() # Abrir pasta, criar arquivo e fechar pasta.
     print(f"Seu arquivo foi adicionado com sucesso!") # Mensagem de confirmação ao usuário
 
-def remover_documento():
-    arquivo = input("Digite o nome do arquivo que deseja remover (com a extenção desejada): ") # Comando para o usuário escrever o nome do arquivo que será removido (com a extenção)
-    os.remove(f"biblioteca/{arquivo}") # Remover o arquivo com base em seu caminho
-    print(f"Seu arquivo foi removido com sucesso!") # Mensagem de confirmação ao usuário
+def remover_documento(): # Função para remover um arquivo
+    arquivo = input("Digite o nome do arquivo que deseja remover (com a extenção desejada) ou digite 'Menu' para retornar ao menu principal: ") # Comando para o usuário escrever o nome do arquivo que será removido (com a extenção)
+    if arquivo.strip().lower() == "menu":
+        return menu()
+    
+    caminho = f"biblioteca/{arquivo}"
+    if not os.path.exists(caminho):
+        print("Não foi possível encontrar o arquivo indicado. Verifique e tente novamente")
+        return remover_documento()
+    else:        
+        os.remove(caminho) # Remover o arquivo com base em seu caminho
+        print(f"Seu arquivo foi removido com sucesso!") # Mensagem de confirmação ao usuário
 
 def menu(): # Função com o menu de navegação
     while True:
